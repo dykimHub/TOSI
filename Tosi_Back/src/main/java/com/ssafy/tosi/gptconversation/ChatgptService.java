a6880cea26ceed9f46239226889a84f6517fb3df
천우진 <qpqpqp0102@naver.com>
2024-01-31 15:37:46 +0900

    Restore
    Change-Id: Ie602fc09fff85c4997e41d87240b2ad31f3508bd

--- Tosi_Back/src/main/java/com/ssafy/tosi/gptconversation/ChatgptService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.tosi.gptconversation;
-
-import io.github.flashvayne.chatgpt.dto.chat.MultiChatMessage;
-
-import java.util.List;
-
-public interface ChatgptService {
-    String sendChat(List<MultiChatMessage> messages);
-}
--- Tosi_Back/src/main/java/com/ssafy/tosi/gptconversation/dto/Message.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.tosi.gptconversation.dto;
-
-import lombok.Data;
-
-@Data
-public class Message {
-    private String role;
-    private String message;
-}
