58af0c0a160f51dadf7f0f3d4323e307e051e625
천우진 <qpqpqp0102@naver.com>
2024-02-02 07:11:12 +0900

    Remove: Back Files
    Change-Id: I856960c68d022765d09bd8992d4fb870d06109c2

--- Tosi_Back/src/main/java/com/ssafy/tosi/favorite/FavoriteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.tosi.favorite;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
-    List<Favorite> findByUserId(Integer userId);
-}
--- Tosi_Back/src/main/java/com/ssafy/tosi/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.tosi.user.repository;
-
-import com.ssafy.tosi.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-}
