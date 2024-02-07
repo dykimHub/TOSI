ca20ed635793775f7ad7729c1862984b2f75756a
우지민 <0542say@naver.com>
2024-02-07 14:28:30 +0900

    Fix : modified file
    Change-Id: Ia4ea263d47fc0c9eac9e32b419c302c63b7aaec1

--- Tosi_Front/src/util/http-common.js
+++ Tosi_Front/src/util/http-common.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
     baseURL: "http://localhost:8080",
     headers: {
         "Content-Type": "application/json",
-    }
+    },
 });
+axios.defaults.withCredentials = true;
