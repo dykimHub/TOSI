5d39689faaaee0de51dc00eb7d16dc00511754f4
우지민 <0542say@naver.com>
2024-02-07 09:25:06 +0900

    Merge file
    Change-Id: I048777c289a1130a3121fa7d9db7bdf44aca496b

--- Tosi_Front/src/components/common/TheSideBar.vue
+++ Tosi_Front/src/components/common/TheSideBar.vue
@@ -2,26 +2,17 @@
     <div>
         <ul>
             <li><RouterLink to="/tales">전체 책 보기</RouterLink></li>
-<<<<<<< HEAD
-            <!-- <li><RouterLink :to="`/favorite/${userId}`">나의 책장</RouterLink></li> -->
-=======
             <li><RouterLink to="/bookshelf">나의 책장</RouterLink></li>
->>>>>>> cda18b08466f9141b772a54bc01811899ff0aa50
             <li><RouterLink to="/customTale/create">나만의 동화 만들기</RouterLink></li>
             <li><RouterLink to="/customtale">친구들의 동화 보기</RouterLink></li>
             <li><RouterLink to="/userInfo">마이페이지</RouterLink></li>
         </ul>
     </div>
 </template>
-<<<<<<< HEAD
 <script setup></script>
-=======
-<script setup>
-</script>
->>>>>>> cda18b08466f9141b772a54bc01811899ff0aa50
 <style scoped>
 div {
-    width:13rem;
+    width: 13rem;
     position: fixed;
     margin-top: 40px;
 }
@@ -32,12 +23,7 @@ li {
     list-style-type: none;
 }
 a {
-<<<<<<< HEAD
     text-decoration: none;
     color: black;
-=======
-text-decoration: none;
-color: black;
->>>>>>> cda18b08466f9141b772a54bc01811899ff0aa50
 }
 </style>
--- Tosi_Front/src/router/index.js
+++ Tosi_Front/src/router/index.js
@@ -22,164 +22,158 @@ import favoriteTaleList from "@/components/bookshelf/favoriteTaleList.vue";
 import favoriteCustomList from "@/components/bookshelf/favoriteCustomList.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/tosi",
-      name: "tosi",
-      component: HomeView,
-      // meta: { requiresAuth: true },
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: "/tales",
-          name: "tales",
-          component: TalesView,
-          meta: { requiresAuth: true },
+            path: "/tosi",
+            name: "tosi",
+            component: HomeView,
+            // meta: { requiresAuth: true },
+            children: [
+                {
+                    path: "/tales",
+                    name: "tales",
+                    component: TalesView,
+                    meta: { requiresAuth: true },
+                },
+                {
+                    path: "/tales/:taleId",
+                    name: "taleDetail",
+                    component: TaleDetail,
+                    props: true,
+                    meta: { requiresAuth: true },
+                },
+                {
+                    path: "/tales/play/:speaker",
+                    name: "talePlay",
+                    component: TalePlay,
+                    props: true,
+                },
+                {
+                    path: "/tales/end/:taleId",
+                    name: "taleEnd",
+                    component: TaleEnd,
+                    props: true,
+                },
+                {
+                    path: "/customTale",
+                    name: "customTale",
+                    component: CustomTaleView,
+                    meta: { requiresAuth: true },
+                    children: [
+                        {
+                            path: "",
+                            name: "customTaleList",
+                            component: CustomTaleList,
+                            meta: { requiresAuth: true },
+                        },
+                        {
+                            path: "create",
+                            name: "customTaleCreate",
+                            component: CustomTaleCreate,
+                            meta: { requiresAuth: true },
+                        },
+                        {
+                            path: "save",
+                            name: "customTaleSave",
+                            component: CustomTaleSave,
+                            meta: { requiresAuth: true },
+                            props: true,
+                        },
+                        {
+                            path: ":customTaleId",
+                            name: "customTaleDetail",
+                            component: CustomTaleDetail,
+                            meta: { requiresAuth: true },
+                        },
+                        {
+                            path: "loading",
+                            name: "loading",
+                            component: LoadingModal,
+                            meta: { requiresAuth: true },
+                        },
+                    ],
+                },
+                {
+                    path: "/tales/chat",
+                    name: "gptConversationView",
+                    component: gptConversationView,
+                    meta: { requiresAuth: true },
+                },
+                {
+                    path: "/tales/end/chat/:cname/:bname",
+                    name: "gptConversationSend",
+                    component: gptConversationSend,
+                    meta: { requiresAuth: true },
+                    props: true,
+                },
+                {
+                    path: "/login",
+                    name: "login",
+                    component: LoginView,
+                    meta: { requiresGuest: true },
+                },
+                {
+                    path: "/regist",
+                    name: "regist",
+                    component: RegistView,
+                    meta: { requiresGuest: true },
+                },
+                {
+                    path: "/userInfo",
+                    name: "userInfo",
+                    component: UserInfoView,
+                    meta: { requiresAuth: true },
+                },
+                {
+                    path: "/userInfo/update",
+                    name: "userInfoUpdate",
+                    component: UserInfoUpdate,
+                    meta: { requiresAuth: true },
+                },
+                {
+                    path: "/bookshelf",
+                    name: "bookshelf",
+                    component: BookshelfView,
+                    meta: { requiresAuth: true },
+                    children: [
+                        {
+                            path: "",
+                            name: "favoriteTaleList",
+                            component: favoriteTaleList,
+                            meta: { requiresAuth: true },
+                        },
+                        {
+                            path: "custom",
+                            name: "favoriteCustomList",
+                            component: favoriteCustomList,
+                            meta: { requiresAuth: true },
+                        },
+                    ],
+                },
+            ],
         },
         {
-          path: "/tales/:taleId",
-          name: "taleDetail",
-          component: TaleDetail,
-          props: true,
-          meta: { requiresAuth: true },
+            path: "/",
+            name: "NonMemberMainView",
+            component: NonMemberMainView,
+            meta: { requiresGuest: true },
         },
-        {
-          path: "/tales/play/:speaker",
-          name: "talePlay",
-          component: TalePlay,
-          props: true,
-        },
-        {
-          path: "/tales/end/:taleId",
-          name: "taleEnd",
-          component: TaleEnd,
-          props: true,
-        },
-<<<<<<< HEAD
     ],
-});
-export default router;
-=======
-        {
-          path: "/customTale",
-          name: "customTale",
-          component: CustomTaleView,
-          meta: { requiresAuth: true },
-          children: [
-            {
-              path: "",
-              name: "customTaleList",
-              component: CustomTaleList,
-              meta: { requiresAuth: true },
-            },
-            {
-              path: "create",
-              name: "customTaleCreate",
-              component: CustomTaleCreate,
-              meta: { requiresAuth: true },
-            },
-            {
-              path: "save",
-              name: "customTaleSave",
-              component: CustomTaleSave,
-              meta: { requiresAuth: true },
-              props: true,
-            },
-            {
-              path: ":customTaleId",
-              name: "customTaleDetail",
-              component: CustomTaleDetail,
-              meta: { requiresAuth: true },
-            },
-            {
-              path: "loading",
-              name: "loading",
-              component: LoadingModal,
-              meta: { requiresAuth: true },
-            }
-          ],
-        },
-        {
-          path: "/tales/chat",
-          name: "gptConversationView",
-          component: gptConversationView,
-          meta: { requiresAuth: true },
-        },
-        {
-          path: "/tales/end/chat/:cname/:bname",
-          name: "gptConversationSend",
-          component: gptConversationSend,
-          meta: { requiresAuth: true },
-          props: true,
-        },
-        {
-          path: "/login",
-          name: "login",
-          component: LoginView,
-          meta: { requiresGuest: true },
-        },
-        {
-          path: "/regist",
-          name: "regist",
-          component: RegistView,
-          meta: { requiresGuest: true },
-        },
-        {
-          path: "/userInfo",
-          name: "userInfo",
-          component: UserInfoView,
-          meta: { requiresAuth: true },
-        },
-        {
-          path: "/userInfo/update",
-          name: "userInfoUpdate",
-          component: UserInfoUpdate,
-          meta: { requiresAuth: true },
-        },
-        {
-          path: "/bookshelf",
-          name: "bookshelf",
-          component: BookshelfView,
-          meta: { requiresAuth: true },
-          children: [
-            {
-              path: "",
-              name: "favoriteTaleList",
-              component: favoriteTaleList,
-              meta: { requiresAuth: true },
-            },
-            {
-              path: "custom",
-              name: "favoriteCustomList",
-              component: favoriteCustomList,
-              meta: { requiresAuth: true },
-            },
-          ],
-        },
-      ],
-    },
-    {
-      path: "/",
-      name: "NonMemberMainView",
-      component: NonMemberMainView,
-      meta: { requiresGuest: true },
-    },
-  ],
 });
 
 router.beforeEach((to, from, next) => {
-  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
 
-  if (to.matched.some((record) => record.meta.requiresAuth) && !isLoggedIn) {
-    alert("로그인 페이지로 이동합니다.")
-    next('/login');
-  } else if (to.matched.some((record) => record.meta.requiresGuest) && isLoggedIn) {
-    alert("이미 로그인 되어있습니다.")
-    next('/tosi');
-  } else {
-    next();
-  }
+    if (to.matched.some((record) => record.meta.requiresAuth) && !isLoggedIn) {
+        alert("로그인 페이지로 이동합니다.");
+        next("/login");
+    } else if (to.matched.some((record) => record.meta.requiresGuest) && isLoggedIn) {
+        alert("이미 로그인 되어있습니다.");
+        next("/tosi");
+    } else {
+        next();
+    }
 });
 
 export default router;
->>>>>>> cda18b08466f9141b772a54bc01811899ff0aa50
