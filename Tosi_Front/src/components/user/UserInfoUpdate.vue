5e061fcf4df62db8757db1ae23e5dafc000741ae
천우진 <qpqpqp0102@naver.com>
2024-02-08 12:43:26 +0900

    Fix: userinfoupdate
    Change-Id: I64e161227261d50fbb4ec13b597b2903316ff91f

--- Tosi_Front/src/components/user/UserInfoUpdate.vue
+++ Tosi_Front/src/components/user/UserInfoUpdate.vue
@@ -1,60 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div class="update-form">
-        <form action="" class="form" @submit.prevent="onsubmit">
-            <h1 class="form__title">마이페이지</h1>
-
-            <div class="update-div">
-                <input type="email" class="update-input" placeholder="" v-model="userInfo.email" readonly />
-                <label class="update-label">이메일</label>
-            </div>
-            <div class="update-div">
-                <input type="password" class="update-input" placeholder=" " v-model="userInfo.password" />
-                <label class="update-label">Password</label>
-            </div>
-            <div class="update-div">
-                <input type="text" class="update-input" placeholder=" " v-model="userInfo.bookshelfName" />
-                <label class="update-label">나의 책장 이름</label>
-            </div>
-
-            <ul class="children-ul">
-                <li v-for="(child, index) in userInfo.childrenList" :key="child.index" class="children-li">
-                    {{ child.childName }} - 성별: {{ child.gender }} - 내 아이 여부: {{ child.isMyBaby }}
-                </li>
-            </ul>
-            <div>
-                <div class="regist-div">
-                    <div>
-                        <input type="text" class="regist-input" placeholder=" " v-model="child.childName" />
-                        <label class="regist-label">아이 이름</label>
-                    </div>
-                    <div>
-                        <input type="radio" class="regist-input" placeholder=" " v-model="child.gender" value="0" />
-                        여자
-                        <input type="radio" class="regist-input" placeholder=" " v-model="child.gender" value="1" />
-                        남자
-                        <label class="regist-label">아이 성별</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" class="regist-input" placeholder=" " v-model="child.isMyBaby" />
-                        <label class="regist-label">내 아이 여부</label>
-                    </div>
-                    <button @click="registerChild">등록</button>
-                </div>
-                <div>
-                    <h2>등록된 아이 목록</h2>
-                    <ul>
-                        <li v-for="(child, index) in childrenList" :key="index">
-                            {{ child.childName }} - 성별: {{ child.gender }} - 내 아이 여부: {{ child.isMyBaby }}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <button @click="deleteUserInfo">회원탈퇴</button>
-            <input type="submit" class="update-button" value="update" @click="update" />
-        </form>
-    </div>
-=======
   <div class="update-form">
     <form action="" class="form" @submit.prevent="onsubmit">
       <div>
@@ -118,7 +62,6 @@
     </form>
 
   </div>
->>>>>>> frontend/feature/newuser
 </template>
 
 <script setup>
@@ -127,44 +70,6 @@ import { useUserStore } from "@/stores/userStore";
 import ChildItem from "@/components/user/ChildItem.vue";
 
 const store = useUserStore();
-<<<<<<< HEAD
-const userInfo = ref({
-    email: store.userInfo.email,
-    bookshelfName: store.userInfo.bookShelfName,
-    childrenList: store.userInfo.childrenList,
-});
-const child = ref({ childName: "", gender: 0, isMyBaby: false });
-const childrenList = ref(userInfo.value.childrenList);
-const isLoggedIn = ref("");
-
-onMounted(() => {
-    isLoggedIn.value = localStorage.getItem("isLoggedIn");
-    console.log("Is Authenticated:", isLoggedIn.value);
-    store.getUser();
-    const user = store.userInfo;
-    userInfo.value.email = user.email;
-    userInfo.value.bookShelfName = user.bookShelfName;
-    userInfo.value.childrenList = user.childrenList;
-});
-
-const registerChild = function () {
-    childrenList.value.push({ ...child.value });
-    console.log(childrenList.value);
-    child.value.childName = "";
-};
-
-const update = function () {
-    store.updateUser(userInfo.value);
-};
-
-const deleteUserInfo = function () {
-    console.log(userInfo.value.email);
-    store.deleteUser(userInfo.value.email);
-    userInfo.value = null;
-    localStorage.removeItem("loginUser");
-    alert("회원탈퇴 완료.");
-};
-=======
 const userInfo = ref({ email: '', bookshelfName: '', childrenList: [] });
 const child = ref({ childName: '', gender: 0, myBaby: false });
 const childrenList = ref([]);
