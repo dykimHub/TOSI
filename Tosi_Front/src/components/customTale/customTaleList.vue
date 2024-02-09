ff27764c30b28ba4c52f7e2ca86aa013dbfbd4dc
양성주 <ztjdwnz@naver.com>
2024-02-10 00:02:40 +0900

    Design: customTale CSS
    Change-Id: Icddae2c901364546cac56968d093c2c9f82d52b5

--- Tosi_Front/src/components/customTale/customTaleList.vue
+++ Tosi_Front/src/components/customTale/customTaleList.vue
@@ -1,39 +1,60 @@
 <template>
-<div class="talelistContainer">
-    <div class = topOfTaleList>
-      <h2>친구들의 동화 보기</h2>
+  <div class="talelistContainer">
+    <div>
+      <div class="title">친구들의 동화 보기</div>
     </div>
     <div class="taleContainer">
-        <ul v-for="customTale in currentPageBoardList"
-      :key="customTale.customTaleId">
-            <div class="oneTale">
-                <RouterLink :to="`/customtale/${customTale.customTaleId}`">
-                  <img class="thumbnail" :src="customTale.thumbnail" /></RouterLink>
-                <br>
-                <RouterLink :to="`/customtale/${customTale.customTaleId}`">{{ customTale.title }}</RouterLink>
-                <br>
-                재생 시간: {{ customTale.time }}
-            </div>
-        </ul>
+      <ul
+        v-for="customTale in currentPageBoardList"
+        :key="customTale.customTaleId"
+      >
+        <div class="oneTale">
+          <RouterLink :to="`/customtale/${customTale.customTaleId}`">
+            <img class="thumbnail" :src="customTale.thumbnail"
+          /></RouterLink>
+          <br />
+          <RouterLink :to="`/customtale/${customTale.customTaleId}`">{{
+            customTale.title
+          }}</RouterLink>
+        </div>
+      </ul>
     </div>
 
     <div>
-      <nav aria-label="Page navigation" style="padding: 15px;">
-      <ul class="pagination d-flex justify-content-center flex-wrap pagination-rounded-flat pagination-success ">
-        <li class="page-item">
-          <a class="page-link" :class="{ disabled: currentPage <= 1 }" href="#" @click.prevent="currentPage--">&lt;</a>
-        </li>
-        <li :class="{ active: currentPage === page }" class="page-item" v-for="page in pageCount" :key="page">
-          <a class="page-link" href="#" @click.prevent="clickPage(page)">{{
-            page
-          }}</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" :class="{ disabled: currentPage >= pageCount }" href="#"
-            @click.prevent="currentPage++">&gt;</a>
-        </li>
-      </ul>
-    </nav>
+      <nav aria-label="Page navigation" style="padding: 15px">
+        <ul
+          class="pagination d-flex justify-content-center flex-wrap pagination-rounded-flat pagination-success"
+        >
+          <li class="page-item">
+            <a
+              class="page-link"
+              :class="{ disabled: currentPage <= 1 }"
+              href="#"
+              @click.prevent="currentPage--"
+              >&lt;</a
+            >
+          </li>
+          <li
+            :class="{ active: currentPage === page }"
+            class="page-item"
+            v-for="page in pageCount"
+            :key="page"
+          >
+            <a class="page-link" href="#" @click.prevent="clickPage(page)">{{
+              page
+            }}</a>
+          </li>
+          <li class="page-item">
+            <a
+              class="page-link"
+              :class="{ disabled: currentPage >= pageCount }"
+              href="#"
+              @click.prevent="currentPage++"
+              >&gt;</a
+            >
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -62,52 +83,60 @@ const currentPageBoardList = computed(() => {
   );
 }); //page
 
-
-
 onMounted(() => {
   customTaleStore.getCustomTalesList(true);
 });
 </script>
 
-<style>
+<style scoped>
 .taleContainer {
-    display: flex; 
-    justify-content: center;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: space-around;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
 }
-
-.talelistContainer{
-  background-color:white;
+.title {
+  text-decoration: none;
+  display: inline-block;
+  box-shadow: inset 0 -20px 0 #d3e4ff;
+  font-size: 40px;
+  margin: 30px 0px 30px 50px;
+  line-height: 1;
+  text-align: left;
+}
+.talelistContainer {
+  background-color: white;
   border-radius: 20px;
   margin: 35px;
-  padding-top: 40px;
-  opacity: 0.95;  
+  /* padding-top: 40px;
+  padding-bottom: 40px; */
+  opacity: 0.95;
+  padding: 40px 60px;
+  border: 5px solid #cee8e8;
 }
-
 .thumbnail {
-    width: 200px;
+  width: 200px;
 }
 
 .oneTale {
-    width: 13em;
-    text-align: center;
-    margin: 2em;
+  width: 13em;
+  text-align: center;
+  margin: 2em;
 }
 
 .selecSort {
-    display: flex;
-    flex-direction: column;
-    margin: 1em;
+  display: flex;
+  flex-direction: column;
+  margin: 1em;
 }
 
-.topOfTaleList{
+/* .topOfTaleList{
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-}
+} */
 
 a {
   text-decoration: none !important;
@@ -115,39 +144,34 @@ a {
 }
 
 .flex {
-     -webkit-box-flex: 1;
-     -ms-flex: 1 1 auto;
-     flex: 1 1 auto
- }
-
-
- /* page */
- .pagination,
- .jsgrid .jsgrid-pager {
-     display: flex;
-     padding-left: 0;
-     list-style: none;
-     border-radius: 0.25rem
- }
- 
- .page-link {
-     color: black
- }
-
- .pagination.pagination-rounded-flat .page-item {
-     margin: 0 .30rem
- }
- 
-
- .pagination-success .page-item.active .page-link
-  {
-     background: #d8eef2;
- }
- 
- .pagination.pagination-rounded-flat .page-item .page-link{
-    border: none;
-    border-radius: 50px;
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
 }
 
+/* page */
+.pagination,
+.jsgrid .jsgrid-pager {
+  display: flex;
+  padding-left: 0;
+  list-style: none;
+  border-radius: 0.25rem;
+}
 
+.page-link {
+  color: black;
+}
+
+.pagination.pagination-rounded-flat .page-item {
+  margin: 0 0.3rem;
+}
+
+.pagination-success .page-item.active .page-link {
+  background: #d8eef2;
+}
+
+.pagination.pagination-rounded-flat .page-item .page-link {
+  border: none;
+  border-radius: 50px;
+}
 </style>
