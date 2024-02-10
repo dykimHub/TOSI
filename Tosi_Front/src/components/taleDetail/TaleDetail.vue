e486b3a4ef634c5055f62729ab0c1edc9050aced
dykim <kdayun@daum.net>
2024-02-10 22:30:44 +0900

    Feat: audio pause
    Change-Id: Ibeddcbeba4b19ab73356c1e03ba71e8699eb98a6

--- Tosi_Front/src/components/taleDetail/TaleDetail.vue
+++ Tosi_Front/src/components/taleDetail/TaleDetail.vue
@@ -1,68 +1,76 @@
 <template>
   <div v-if="taleDetailStore.tale">
-      <div class="play">
-          <div class="book">
-              <div class="info">
-                  <div class="title">{{ taleDetailStore.tale.title }}</div>
-              </div>
-              <div class="container">
-                  <div class="imgNselect leftImg">
-                      <img :src="taleDetailStore.tale.thumbnail" />
-                  </div>
-                  <div class="imgNselect">
-                      <div class="selected">
-                          <h1>오늘의 주인공</h1>
-                          <div class="selectbox">
-                              <select
-                                  class="form-select"
-                                  aria-label="Default select example"
-                                  v-model="selectedCharacter"
-                              >
-                                  <option
-                                      v-for="character in taleDetailStore.tale.characters"
-                                      :key="character"
-                                      :value="character"
-                                      :disabled="isCharacterSelected(character)"
-                                      class="character"
-                                  >
-                                      {{ character }}
-                                  </option>
-                              </select>
-                              <select class="form-select" aria-label="Default select example" v-model="selectedName">
-                                  <option v-for="(child, index) in userStore.userInfo.childrenList" :key="index">
-                                      {{ child.childName }}
-                                  </option>
-                              </select>
-                          </div>
-                          <div v-for="(name, index) in nameMap" :key="index">
-                              <h2 class="nameMap">
-                                  {{ name[0] }} -> {{ name[1] }} <button @click="deleteName(index)">삭제</button>
-                              </h2>
-                          </div>
-                      </div>
-                      <h1>목소리 선택</h1>
-                      <div class="align-items-center grid-container">
-                          <div v-for="item in items" :key="item.speaker" class="form-wrapper align-items-center">
-                              <label
-                                  ><input type="radio" :value="item.speaker" v-model="speaker" :name="item.name" />
-                                  {{ item.name }}
-                                  <img
-                                      src="https://talebucket.s3.ap-northeast-2.amazonaws.com/volume_up_FILL0_wght400_GRAD0_opsz24.svg"
-                                      alt="Speaker Image"
-                                      class="speaker-image"
-                                      @click="playVoice(item.url)"
-                                  />
-                              </label>
-                          </div>
-                      </div>
-                      <button class="btn btn-primary" type="submit" @click="readBook">동화 시작하기</button>
-                  </div>
-              </div>
+    <div class="play">
+      <div class="book">
+        <div class="info">
+          <div class="title">{{ taleDetailStore.tale.title }}</div>
+        </div>
+        <div class="container">
+          <div class="imgNselect leftImg">
+            <img :src="taleDetailStore.tale.thumbnail" />
           </div>
+          <div class="imgNselect">
+            <div class="selected">
+              <h1>오늘의 주인공</h1>
+              <div class="selectbox">
+                <select
+                  class="form-select"
+                  aria-label="Default select example"
+                  v-model="selectedCharacter"
+                >
+                  <option
+                    v-for="character in taleDetailStore.tale.characters"
+                    :key="character"
+                    :value="character"
+                    :disabled="isCharacterSelected(character)"
+                    class="character"
+                  >
+                    {{ character }}
+                  </option>
+                </select>
+                <select
+                  class="form-select"
+                  aria-label="Default select example"
+                  v-model="selectedName"
+                >
+                  <option v-for="(child, index) in userStore.userInfo.childrenList" :key="index">
+                    {{ child.childName }}
+                  </option>
+                </select>
+              </div>
+              <div v-for="(name, index) in nameMap" :key="index">
+                <h2 class="nameMap">
+                  {{ name[0] }} -> {{ name[1] }} <button @click="deleteName(index)">삭제</button>
+                </h2>
+              </div>
+            </div>
+            <h1>목소리 선택</h1>
+            <div class="align-items-center grid-container">
+              <div
+                v-for="item in items"
+                :key="item.speaker"
+                class="form-wrapper align-items-center"
+              >
+                <label
+                  ><input type="radio" :value="item.speaker" v-model="speaker" :name="item.name" />
+                  {{ item.name }}
+                  <img
+                    src="https://talebucket.s3.ap-northeast-2.amazonaws.com/volume_up_FILL0_wght400_GRAD0_opsz24.svg"
+                    alt="Speaker Image"
+                    class="speaker-image"
+                    @click="playVoice(item.url)"
+                  />
+                </label>
+              </div>
+            </div>
+            <button class="btn btn-primary" type="submit" @click="readBook">동화 시작하기</button>
+          </div>
+        </div>
       </div>
+    </div>
   </div>
   <div v-else>
-      <div>is Loading...</div>
+    <div>is Loading...</div>
   </div>
 </template>
 <script setup>
@@ -84,17 +92,41 @@ console.log(userStore.userInfo);
 //tts
 const speaker = ref("vdain");
 const items = ref([
-  { name: "다인", speaker: "vdain", url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vdain.mp3" },
-  { name: "고은", speaker: "vgoeun", url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vgoeun.mp3" },
-  { name: "미경", speaker: "vmikyung", url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vmikyung.mp3" },
-  { name: "이안", speaker: "vian", url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vian.mp3" },
-  { name: "대성", speaker: "vdaeseong", url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vdaeseong.mp3" },
-  { name: "원탁", speaker: "nwontak", url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/nwontak.mp3" },
+  {
+    name: "다인",
+    speaker: "vdain",
+    url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vdain.mp3",
+  },
+  {
+    name: "고은",
+    speaker: "vgoeun",
+    url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vgoeun.mp3",
+  },
+  {
+    name: "미경",
+    speaker: "vmikyung",
+    url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vmikyung.mp3",
+  },
+  {
+    name: "이안",
+    speaker: "vian",
+    url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vian.mp3",
+  },
+  {
+    name: "대성",
+    speaker: "vdaeseong",
+    url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/vdaeseong.mp3",
+  },
+  {
+    name: "원탁",
+    speaker: "nwontak",
+    url: "https://talebucket.s3.ap-northeast-2.amazonaws.com/nwontak.mp3",
+  },
 ]);
 const audioRef = ref(null);
 const playVoice = (url) => {
   if (audioRef.value != null) {
-      audioRef.value.pause();
+    audioRef.value.pause();
   }
   const audio = new Audio(url);
   audioRef.value = audio;
@@ -106,10 +138,10 @@ const selectedName = ref();
 const nameMap = ref([]);
 watch([selectedCharacter, selectedName], ([newCharacter, newName]) => {
   if (newCharacter && newName) {
-      nameMap.value.push([newCharacter, newName]);
-      console.log(nameMap.value);
-      selectedCharacter.value = null;
-      selectedName.value = null;
+    nameMap.value.push([newCharacter, newName]);
+    console.log(nameMap.value);
+    selectedCharacter.value = null;
+    selectedName.value = null;
   }
 });
 function isCharacterSelected(character) {
@@ -120,26 +152,26 @@ function deleteName(index) {
 }
 const readBook = async () => {
   try {
-      console.log("요청 보냄");
-      // 이름 바꾸기
-      for (const [cname, bname] of nameMap.value) {
-          axios.post(`http://localhost:8080/tales/${cname}/${bname}`);
-      }
-      const response = await axios.post("http://localhost:8080/tales/read", taleDetailStore.tale);
-      taleDetailStore.pages = response.data;
-      // 요청이 성공적으로 완료된 후에 navigateToTalePlay 호출
-      console.log("요청이 성공적으로 완료된 후에 navigateToTalePlay 호출");
-      navigateToTalePlay();
+    console.log("요청 보냄");
+    // 이름 바꾸기
+    for (const [cname, bname] of nameMap.value) {
+      axios.post(`http://localhost:8080/tales/${cname}/${bname}`);
+    }
+    const response = await axios.post("http://localhost:8080/tales/read", taleDetailStore.tale);
+    taleDetailStore.pages = response.data;
+    // 요청이 성공적으로 완료된 후에 navigateToTalePlay 호출
+    console.log("요청이 성공적으로 완료된 후에 navigateToTalePlay 호출");
+    navigateToTalePlay();
   } catch (error) {
-      console.error("Error fetching:", error);
+    console.error("Error fetching:", error);
   }
 };
 const navigateToTalePlay = () => {
   console.log("다음페이지 보내기");
   const selectedSpeaker = items.value.find((item) => item.speaker === speaker.value);
   router.push({
-      name: "talePlay",
-      params: { speaker: selectedSpeaker.speaker },
+    name: "talePlay",
+    params: { speaker: selectedSpeaker.speaker },
   });
 };
 </script>
