6db9cceb3f0966c49c7230373467d306212ea567
김다윤 <dy.jo.kim@gmail.com>
2024-09-08 15:42:32 +0900

    Init: 커스텀 동화 프로젝트 생성

---
+++ backend/customTale/src/main/java/com/tosi/customtale/config/S3Config.java
@@ -0,0 +1,49 @@
+package com.ssafy.tosi.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    //@Value("${cloud.aws.credentials.access-key}")
+    private String accessKey = "AKIAQ3EGUWZYISDXIGFJ";
+
+    //@Value("${cloud.aws.credentials.seacret-key}")
+    private String secretKey="pnbbihQ2azlBemvI9BwY46ob6YgRHqqDbjOsgpv5";
+
+    //@Value("${cloud.aws.region.static}")
+    private String region="ap-northeast-2";
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        AwsCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+}
